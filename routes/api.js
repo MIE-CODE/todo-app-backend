2d915366c60c64d03dcb7082c5399017d261634a
Meyangz <Kayfolayan614@gmail.com>
2024-05-12 12:04:39 +0100

    modified the routes

--- routes/api.js
+++ routes/api.js
@@ -34,6 +34,7 @@ router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.login(email, password);
+    res.status(200).json({ user: user._id });
   } catch (error) {
     res.status(400).json({});
   }
